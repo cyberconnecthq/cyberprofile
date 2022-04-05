1c3def3d721b1de19838024fba87e242a53a9c14
Ryan Li <ryan@ryanli.me>
2022-04-05 15:24:23 -0700

    Finish support ens avatar text record metadata

--- src/utils/resolver.ts
+++ src/utils/resolver.ts
@@ -1,12 +1,15 @@
 import { Resolver, StaticJsonRpcProvider } from "@ethersproject/providers";
 import { Avatar } from "@ethersproject/providers/lib/base-provider";
-import { hexConcat, hexDataSlice, hexZeroPad, isHexString } from "@ethersproject/bytes";
+import {
+  hexConcat,
+  hexDataSlice,
+  hexZeroPad,
+  isHexString,
+} from "@ethersproject/bytes";
 import { BigNumber } from "@ethersproject/bignumber";
 import { toUtf8String } from "@ethersproject/strings";
 import { fetchJson } from "@ethersproject/web";
 import { namehash } from "@ethersproject/hash";
-
-
 import { Logger } from "@ethersproject/logger";
 // Forked on 4/5/2022
 const version = "providers/5.6.2";
@@ -56,11 +59,27 @@ function _parseString(result: string, start: number): null | string {
   return null;
 }
 
+type CyberAvatarExtra = {
+  name: string;
+  type: "uri:https" | "uri:data" | "uri:ipfs" | "nft:erc721" | "nft:erc1155";
+  record: string;
+  owner?: string;
+  balance?: string;
+  owned?: boolean;
+  metadataUrlBase?: string;
+  metadataUrlExpanded?: string;
+  metadataUrl?: string;
+  metadata?: string;
+  metadataImageUrlIPFS?: string;
+  metadataImageUrl?: string;
+};
+type CyberAvatar = {
+  extra: CyberAvatarExtra;
+  url: string;
+};
+
 export class CyberResolver extends Resolver {
-  async getAvatar(): Promise<null | Avatar> {
-    const linkage: Array<{ type: string; content: string }> = [
-      { type: "name", content: this.name },
-    ];
+  async getCyberAvatar(): Promise<null | CyberAvatar> {
     try {
       // test data for ricmoo.eth
       //const avatar = "eip155:1/erc721:0x265385c7f4132228A0d54EB1A9e7460b91c0cC68/29233";
@@ -79,22 +98,44 @@ export class CyberResolver extends Resolver {
 
         switch (scheme) {
           case "https":
-            linkage.push({ type: "url", content: avatar });
-            return { linkage, url: avatar };
+            return {
+              extra: {
+                name: this.name,
+                type: "uri:https",
+                record: avatar,
+              },
+              url: avatar,
+            };
 
           case "data":
-            linkage.push({ type: "data", content: avatar });
-            return { linkage, url: avatar };
+            return {
+              extra: {
+                name: this.name,
+                type: "uri:data",
+                record: avatar,
+              },
+              url: avatar,
+            };
 
           case "ipfs":
-            linkage.push({ type: "ipfs", content: avatar });
-            return { linkage, url: getIpfsLink(avatar) };
+            return {
+              extra: {
+                name: this.name,
+                type: "uri:ipfs",
+                record: avatar,
+              },
+              url: avatar,
+            };
 
           case "erc721":
           case "erc1155": {
             // Depending on the ERC type, use tokenURI(uint256) or url(uint256)
             const selector = scheme === "erc721" ? "0xc87b56dd" : "0x0e89341c";
-            linkage.push({ type: scheme, content: avatar });
+            const extra: CyberAvatarExtra = {
+              name: this.name,
+              type: `nft:${scheme}`,
+              record: avatar,
+            };
 
             // The owner of this name
             const owner = this._resolvedAddress || (await this.getAddress());
@@ -119,10 +160,8 @@ export class CyberResolver extends Resolver {
                   data: hexConcat(["0x6352211e", tokenId]),
                 })
               );
-            //   if (owner !== tokenOwner) {
-            //     return null;
-            //   }
-              linkage.push({ type: "owner", content: tokenOwner });
+              extra.owner = tokenOwner;
+              extra.owned = owner === tokenOwner;
             } else if (scheme === "erc1155") {
               // balanceOf(address owner, uint256 tokenId)
               const balance = BigNumber.from(
@@ -135,10 +174,8 @@ export class CyberResolver extends Resolver {
                   ]),
                 })
               );
-            //   if (balance.isZero()) {
-            //     return null;
-            //   }
-              linkage.push({ type: "balance", content: balance.toString() });
+              extra.balance = balance.toString();
+              extra.owned = !balance.isZero();
             }
 
             // Call the token contract for the metadata URL
@@ -151,15 +188,12 @@ export class CyberResolver extends Resolver {
             if (metadataUrl == null) {
               return null;
             }
-            linkage.push({ type: "metadata-url-base", content: metadataUrl });
+            extra.metadataUrlBase = metadataUrl;
 
             // ERC-1155 allows a generic {id} in the URL
             if (scheme === "erc1155") {
               metadataUrl = metadataUrl.replace("{id}", tokenId.substring(2));
-              linkage.push({
-                type: "metadata-url-expanded",
-                content: metadataUrl,
-              });
+              extra.metadataUrlExpanded = metadataUrl;
             }
 
             // Transform IPFS metadata links
@@ -167,17 +201,14 @@ export class CyberResolver extends Resolver {
               metadataUrl = getIpfsLink(metadataUrl);
             }
 
-            linkage.push({ type: "metadata-url", content: metadataUrl });
+            extra.metadataUrl = metadataUrl;
 
             // Get the token metadata
             const metadata = await fetchJson(metadataUrl);
             if (!metadata) {
               return null;
             }
-            linkage.push({
-              type: "metadata",
-              content: JSON.stringify(metadata),
-            });
+            extra.metadata = JSON.stringify(metadata);
 
             // Pull the image URL out
             let imageUrl = metadata.image;
@@ -194,13 +225,13 @@ export class CyberResolver extends Resolver {
                 return null;
               }
 
-              linkage.push({ type: "url-ipfs", content: imageUrl });
+              extra.metadataImageUrlIPFS = imageUrl;
               imageUrl = getIpfsLink(imageUrl);
             }
 
-            linkage.push({ type: "url", content: imageUrl });
+            extra.metadataImageUrl = imageUrl;
 
-            return { linkage, url: imageUrl };
+            return { extra, url: imageUrl };
           }
         }
       }
@@ -210,84 +241,100 @@ export class CyberResolver extends Resolver {
   }
 }
 
-
 export class CyberProvider extends StaticJsonRpcProvider {
-    async getResolver(name: string): Promise<null | Resolver> {
-        let currentName = name;
-        while (true) {
-            if (currentName === "" || currentName === ".") { return null; }
+  async getResolver(name: string): Promise<null | Resolver> {
+    let currentName = name;
+    while (true) {
+      if (currentName === "" || currentName === ".") {
+        return null;
+      }
 
-            // Optimization since the eth node cannot change and does
-            // not have a wildcar resolver
-            if (name !== "eth" && currentName === "eth") { return null; }
+      // Optimization since the eth node cannot change and does
+      // not have a wildcar resolver
+      if (name !== "eth" && currentName === "eth") {
+        return null;
+      }
 
-            // Check the current node for a resolver
-            const addr = await this._getResolver(currentName, "getResolver");
+      // Check the current node for a resolver
+      const addr = await this._getResolver(currentName, "getResolver");
 
-            // Found a resolver!
-            if (addr != null) {
-                const resolver = new CyberResolver(this, addr, name);
+      // Found a resolver!
+      if (addr != null) {
+        const resolver = new CyberResolver(this, addr, name);
 
-                // Legacy resolver found, using EIP-2544 so it isn't safe to use
-                if (currentName !== name && !(await resolver.supportsWildcard())) { return null; }
-
-                return resolver;
-            }
-
-            // Get the parent node
-            currentName = currentName.split(".").slice(1).join(".");
+        // Legacy resolver found, using EIP-2544 so it isn't safe to use
+        if (currentName !== name && !(await resolver.supportsWildcard())) {
+          return null;
         }
 
+        return resolver;
+      }
+
+      // Get the parent node
+      currentName = currentName.split(".").slice(1).join(".");
     }
-    async getAvatar(nameOrAddress: string): Promise<null | string> {
+  }
+  async getCyberAvatar(nameOrAddress: string): Promise<null | CyberAvatar> {
+    // @ts-ignore
+    let resolver: CyberResolver = null;
+    if (isHexString(nameOrAddress)) {
+      // Address; reverse lookup
+      const address = this.formatter.address(nameOrAddress);
+
+      const node = address.substring(2).toLowerCase() + ".addr.reverse";
+
+      const resolverAddress = await this._getResolver(node, "getAvatar");
+      if (!resolverAddress) {
+        return null;
+      }
+
+      // Try resolving the avatar against the addr.reverse resolver
+      resolver = new CyberResolver(this, resolverAddress, node);
+      try {
+        const avatar = await resolver.getCyberAvatar();
+
+        if (avatar) {
+          return avatar;
+        }
+      } catch (error) {
         // @ts-ignore
-        let resolver: CyberResolver = null;
-        if (isHexString(nameOrAddress)) {
-            // Address; reverse lookup
-            const address = this.formatter.address(nameOrAddress);
-
-            const node = address.substring(2).toLowerCase() + ".addr.reverse";
-
-            const resolverAddress = await this._getResolver(node, "getAvatar");
-            if (!resolverAddress) { return null; }
-
-            // Try resolving the avatar against the addr.reverse resolver
-            resolver = new Resolver(this, resolverAddress, node);
-            try {
-                const avatar = await resolver.getAvatar();
-
-                if (avatar) { return avatar.url; }
-            } catch (error) {
-                // @ts-ignore
-                if (error.code !== Logger.errors.CALL_EXCEPTION) { throw error; }
-            }
-
-            // Try getting the name and performing forward lookup; allowing wildcards
-            try {
-                // keccak("name(bytes32)")
-                const name = _parseString(await this.call({
-                    to: resolverAddress,
-                    data: ("0x691f3431" + namehash(node).substring(2))
-                }), 0);
-                // @ts-ignore
-                resolver = await this.getResolver(name);
-            } catch (error) {
-                // @ts-ignore
-                if (error.code !== Logger.errors.CALL_EXCEPTION) { throw error; }
-                return null;
-            }
-
-        } else {
-            // ENS name; forward lookup with wildcard
-            // @ts-ignore
-            resolver = await this.getResolver(nameOrAddress);
-            if (!resolver) { return null; }
+        if (error.code !== Logger.errors.CALL_EXCEPTION) {
+          throw error;
         }
+      }
 
-        const avatar = await resolver.getAvatar();
-        if (avatar == null) { return null; }
-
-        return avatar.url;
+      // XXX(@ryanli): Not sure what this part does
+      // Try getting the name and performing forward lookup; allowing wildcards
+      try {
+        // keccak("name(bytes32)")
+        const name = _parseString(
+          await this.call({
+            to: resolverAddress,
+            data: "0x691f3431" + namehash(node).substring(2),
+          }),
+          0
+        );
+        // @ts-ignore
+        resolver = await this.getResolver(name);
+      } catch (error) {
+        // @ts-ignore
+        if (error.code !== Logger.errors.CALL_EXCEPTION) {
+          throw error;
+        }
+        return null;
+      }
+    } else {
+      // ENS name; forward lookup with wildcard
+      // @ts-ignore
+      resolver = await this.getResolver(nameOrAddress);
+      if (!resolver) {
+        return null;
+      }
     }
 
+    const avatar = await resolver.getCyberAvatar();
+    logger.warn(avatar);
+
+    return avatar;
+  }
 }
