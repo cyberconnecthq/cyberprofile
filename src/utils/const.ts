ad6573085e3f15cfcda679eddb022e390bc29e81
Jiayi Xin <jiayi920429@gmail.com>
2022-04-22 22:38:19 -0700

    add cyber profile package

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -12,3 +12,103 @@ export const swaggerSpec = {
 };
 
 export const NotFoundError = new Error("Not Found");
+
+export const cyberProfileSchema = {
+  $schema: "http://json-schema.org/draft-07/schema#",
+  title: "CyberProfileSchema",
+  type: "object",
+  properties: {
+    handle: {
+      type: "string",
+    },
+    type: {
+      type: "string",
+      pattern: "(PERSONAL|ORGANIZATION)",
+    },
+    profilePicture: {
+      type: "string",
+    },
+    backgroundPicture: {
+      type: "string",
+    },
+    sector: {
+      type: "string",
+    },
+    network: {
+      type: "array",
+      items: {
+        type: "string",
+      },
+    },
+    blocks: {
+      type: "array",
+      items: {
+        $ref: "#/definitions/block",
+      },
+    },
+  },
+  definitions: {
+    block: {
+      type: "object",
+      properties: {
+        displayName: {
+          type: "string",
+        },
+        type: {
+          $ref: "#/definitions/blockType",
+        },
+        content: {
+          type: "object",
+          properties: {
+            link: {
+              $ref: "#/definitions/link",
+            },
+            links: {
+              type: "array",
+              items: {
+                $ref: "#/definitions/link",
+              },
+            },
+            members: {
+              type: "array",
+              items: {
+                $ref: "#/definitions/member",
+              },
+            },
+          },
+        },
+      },
+    },
+    blockType: {
+      type: "string",
+      pattern: "(LINK|MIRROR|MEDIUM|TWEET|TEAM|BACKER)",
+    },
+    link: {
+      type: "string",
+    },
+    member: {
+      type: "object",
+      properties: {
+        address: {
+          type: "string",
+        },
+        name: {
+          type: "string",
+        },
+        title: {
+          type: "string",
+        },
+        avatar: {
+          type: "string",
+        },
+      },
+    },
+  },
+};
+
+export const cyberProfileDefinition = {
+  name: "Cyber Profile",
+  schema:
+    "ceramic://kjzl6cwe1jw146elfioarvmv6zspy3pkh986ex082bogtuallfmgquik74i3fyr",
+  description: "Cyber profile information",
+};
