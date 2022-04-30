3cd7b9978e7242117bd40f9d2f543469336ef78c
Ryan Li <ryan@ryanli.me>
2022-04-29 17:20:37 -0700

    [fix] swagger schema, again

--- pages/api/profile/[id].ts
+++ pages/api/profile/[id].ts
@@ -5,6 +5,24 @@ import { parseId } from '@/utils/parser'
 import { NotFoundError } from "@/utils/const";
 import { resolveEns } from "@/utils/provider";
 
+export type DataEnsAvatar = {
+  record: string; // the original avatar text record
+  type: "uri:https" | "uri:data" | "uri:ipfs" | "nft:erc721" | "nft:erc1155"; // the type of avatar text record. supports `uri:https`, `uri:data`, `uri:ipfs`, `nft-erc721`, `nft-erc1155`. see https://docs.ens.domains/ens-improvement-proposals/ensip-12-avatar-text-records nftMetadataURL: string | null; // only available if use NFT type record
+  nftMetadata: string | null; // the metadata of that NFT
+  nftOwner: string | null; // the owner of that NFT (ERC721)
+  nftBalance: string | null; // the balance of the address for the NFT (ERC1155)
+  nftOwned: boolean | null; // whether he owns that NFT or not
+  url: string | null; // final resolution
+};
+
+export type Data = {
+  address: string;
+  name: string | null; 
+  primaryName: string | null; // primary name/reverse record on the address. If user input an address, the name and displayName will always match
+  ensAvatar: DataEnsAvatar | null;
+};
+
+
 /**
  * @swagger
  * components:
@@ -45,28 +63,6 @@ import { resolveEns } from "@/utils/provider";
  *              type: string
  *              format: uri
  *              nullable: true
- *
- */
- export type DataEnsAvatar = {
-  record: string; // the original avatar text record
-  type: "uri:https" | "uri:data" | "uri:ipfs" | "nft:erc721" | "nft:erc1155"; // the type of avatar text record. supports `uri:https`, `uri:data`, `uri:ipfs`, `nft-erc721`, `nft-erc1155`. see https://docs.ens.domains/ens-improvement-proposals/ensip-12-avatar-text-records nftMetadataURL: string | null; // only available if use NFT type record
-  nftMetadata: string | null; // the metadata of that NFT
-  nftOwner: string | null; // the owner of that NFT (ERC721)
-  nftBalance: string | null; // the balance of the address for the NFT (ERC1155)
-  nftOwned: boolean | null; // whether he owns that NFT or not
-  url: string | null; // final resolution
-};
-
-export type Data = {
-  address: string;
-  name: string | null; 
-  primaryName: string | null; // primary name/reverse record on the address. If user input an address, the name and displayName will always match
-  ensAvatar: DataEnsAvatar | null;
-};
-
-
-/**
- * @swagger
  * /api/profile/{id}:
  *   get:
  *     tags: [Profile]
